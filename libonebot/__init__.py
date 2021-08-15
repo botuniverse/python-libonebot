bd815817c91362c6de1697bfc1177f87138445b1
Flandre Cirno <35405826+FlandreCirno@users.noreply.github.com>
2021-08-15 04:16:14 -0400

    Added websocket and a simple test

--- libonebot/__init__.py
+++ libonebot/__init__.py
@@ -7,7 +7,7 @@ import logging
 import requests
 import uvicorn
 from typing import Optional, Callable, Awaitable, Coroutine, Iterable
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 from .bus import ActionBus, EventBus
 from .event import Event
@@ -18,7 +18,7 @@ HTTP_HOST = "127.0.0.1"
 HTTP_PORT = 8080
 HTTP_WEBHOOK = "http://127.0.0.1:5700"
 WS_HOST = "127.0.0.1"
-WS_PORT = ""
+WS_PORT = 8080
 WS_REVERSE_HOST = "127.0.0.1"
 WS_REVERSE_PORT = ""
 
@@ -30,6 +30,7 @@ class OneBot:
     ):
         self._server_app = FastAPI()
         self._register_action_handler(CoreActions.values())
+        self._register_websocket()
         self._action_bus = ActionBus()
         self._event_bus = EventBus()
         self._http_session = requests.Session()
@@ -45,6 +46,35 @@ class OneBot:
         self._server_app.post("/")(_action_handler)
         # for action_model in actions:
         #     self._server_app.post("/")
+
+    def _register_websocket(self) -> None:
+        async def websocket_endpoint(websocket: WebSocket):
+            event_queue = asyncio.Queue()
+
+            async def enque_event(*args, **kwargs):
+                await event_queue.put(kwargs)
+
+            self._event_bus.subscribe(enque_event)
+            await websocket.accept()
+            try:
+
+                async def receive():
+                    while True:
+                        act = await websocket.receive_json()
+                        result = await self._action_bus.emit(**act)
+                        result["echo"] = act["echo"]
+                        await websocket.send_json(result)
+
+                async def send():
+                    while True:
+                        eve = await event_queue.get()
+                        await websocket.send_json(eve)
+
+                await asyncio.gather(send(), receive(), return_exceptions=True)
+            except WebSocketDisconnect:
+                self._event_bus.unsubscribe(enque_event)
+
+        self._server_app.websocket("/ws")(websocket_endpoint)
 
     def _register_http_webhook(self, http_host: str) -> None:
         async def send_event(*args, **event_data):
---
+++ testonebotuser.py
@@ -0,0 +1,71 @@
+import asyncio
+import json
+
+from flask import Flask, request
+import websockets
+import requests
+
+EVENT_DATA = {
+    "user_id": "12234",
+    "self_id": "123234",
+    "messsage": "sajghds",
+    "type": "message",
+    "detail_type": "",
+    "sub_type": "",
+    "extended": {"data": "blah"},
+}
+ACTION_DATA1 = {
+    "action": "send_message",
+    "params": {"user_id": "123", "message": "abcc"},
+    "echo": {"id": "1"},
+}
+
+ACTION_DATA2 = {
+    "action": "qq_group_system_message",
+    "params": {"group_id": "123", "message": "abcc"},
+    "echo": {"id": "2"},
+}
+
+ACTION_RESULT1 = {
+    "status": "ok",
+    "retcode": 0,
+    "data": {},
+    "echo": ACTION_DATA1["echo"],
+}
+
+ACTION_RESULT2 = {
+    "status": "ok",
+    "retcode": 0,
+    "data": {},
+    "echo": ACTION_DATA2["echo"],
+}
+
+onebot_user = Flask("user")
+
+
+@onebot_user.route("/", methods=["POST"])
+def event():
+    print(f"Event received {str(request.json)}")
+    assert dict(request.json) == EVENT_DATA
+    return None
+
+
+async def onebot_user_ws_test():
+    async with websockets.connect("ws://127.0.0.1:8080/ws") as onebot_user_ws:
+        await onebot_user_ws.send(json.dumps(ACTION_DATA1))
+        assert json.loads(await onebot_user_ws.recv()) == ACTION_RESULT1
+        await onebot_user_ws.send(json.dumps(ACTION_DATA2))
+        assert json.loads(await onebot_user_ws.recv()) == ACTION_RESULT2
+
+
+def onebot_user_http_test():
+    result = requests.post("http://127.0.0.1:8080", json=ACTION_DATA1)
+    assert result.json() == ACTION_RESULT1
+    result = requests.post("http://127.0.0.1:8080", json=ACTION_DATA2)
+    assert result.json() == ACTION_RESULT2
+
+
+if __name__ == "__main__":
+    asyncio.run(onebot_user_ws_test())
+    onebot_user_http_test()
+    print("Test passed")
